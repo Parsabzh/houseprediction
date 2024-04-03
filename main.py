bdb95e7f692f02391e675c39a6d7f00bd41b958a
parsabzh <parsabzh@gmail.com>
2024-04-03 15:42:29 +0200

    add visualization

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
 from load_data import load_data
+import matplotlib.pyplot as plt
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from scipy.stats import binom
+from split_data import split
 
 housing= load_data()
+
+print(housing.head())
+print(housing.info())
+print(housing.describe())
+
+
+
+IMAGES_PATH = Path() / "images" / "end_to_end_project"
+IMAGES_PATH.mkdir(parents=True, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = IMAGES_PATH / f"{fig_id}.{fig_extension}"
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+    # extra cgit config --global user.emailode – the next 5 lines define the default font sizes
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+housing.hist(bins=50, figsize=(12, 8))
+save_fig("attribute_histogram_plots")  # extra code
+plt.show()
+
+
+train_set, test_set= split(housing)
+
+housing= train_set.copy()
+
+housing.plot(kind="scatter", x='longtitude',y='latitude',grid=True,alpha=0.2)
+save_fig("population_density")
---
+++ split_data.py
@@ -0,0 +1,20 @@
+from scipy.stats import binom
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+#shows how to compute the 10.7% proba of getting a bad sample
+
+sample_size = 1000
+ratio_female = 0.511
+proba_too_small = binom(sample_size, ratio_female).cdf(485-1)
+proba_too_large = 1 - binom(sample_size, ratio_female).cdf(535)
+print(proba_too_small + proba_too_large)
+
+
+# create income category
+def split(data):
+    data["income_cat"]= pd.cut(data["median_income"],bins=[0.,1.5,3.0,4.5,6.,np.inf],
+                               labels=[1,2,3,4,5])
+    train_set,test_set= train_test_split(data,test_size=0.2, random_state=42, stratify=data["income_cat"])
+    return train_set, test_set
